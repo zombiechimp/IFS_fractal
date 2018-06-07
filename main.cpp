69c6c4ec40f1f6d85f494cdc07fb5e93462d7b7a
Dorotea Jakic <dorotea.jakic@fer.hr>
2018-06-07 17:22:28 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IFS-fractal
+//
+//  Created by Tea Jakić on 07/06/2018.
+//  Copyright © 2018 Tea Jakić. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
